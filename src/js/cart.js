fc695d67d1fe8733b937bb8f4fcc3bbe11dbfe73
Somedude5 <parkerhatch2098@gmail.com>
2023-09-30 12:46:08 -0700

    Feature Addition of removing cart items
    Changed files and added the functions setupDeleteBUttons, deleteItemFromCart and added some es lint exceptions for console logs in productDetails, also exported addSuperScript function to allow it to be used in the setupDelete Buttons function.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,6 @@
 import { getLocalStorage } from "./utils.mjs";
+import { addSuperScript } from "./header";
+import { showNotification } from "./productDetails.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || []; // Default to an empty array if null
@@ -6,7 +8,9 @@ function renderCartContents() {
   if (Array.isArray(cartItems)) { // Check if cartItems is an array
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    setupDeleteButtons(); // listens for the x delete button to be hit to trigger deleting the item from the cart
   } else {
+    // eslint-disable-next-line no-console
     console.log("Expected cartItems to be an array, but got:", cartItems);
   }
 }
@@ -15,6 +19,7 @@ function renderCartContents() {
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
+    <button class="deleteFromCart" data-id="${item.Id}" name-id="${item.Name}" >X</button>
     <img
       src="${item.Image}"
       alt="${item.Name}"
@@ -29,6 +34,32 @@ function cartItemTemplate(item) {
 </li>`;
 
   return newItem;
+}
+
+function setupDeleteButtons() {
+  const deleteButtons = document.querySelectorAll(".deleteFromCart");
+  deleteButtons.forEach(button => {
+    button.addEventListener("click", function(event) {
+      // prevents the default event from occuring when the delete button is clicked
+      event.preventDefault();
+      const itemId = event.target.getAttribute("data-id");
+      const itemName = event.target.getAttribute("name-id")
+      deleteItemFromCart(itemId);
+      showNotification(`Item: ${itemName} has been deleted from your cart`);
+      renderCartContents();
+      // updates the cart superscript icon value after items have been removed
+      addSuperScript()
+    });
+  });
+}
+
+// Function to update the local storage and delete the item IDs
+function deleteItemFromCart(itemId) {
+
+  // This filters out the item with the given ID, deleting it from the cart
+  let cartItems = getLocalStorage("so-cart") || [];
+  cartItems = cartItems.filter(item => item.Id !== itemId);
+  localStorage.setItem("so-cart", JSON.stringify(cartItems));
 }
 
 renderCartContents();
