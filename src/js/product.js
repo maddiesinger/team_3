4872fbb017e5b15d7222a564339d131796c3e406
Amethyst Gilborn <jujaritchie@gmail.com>
2023-10-02 17:11:34 -0600

    Animated backpack when cart button is pushed

--- src/js/product.js
+++ src/js/product.js
@@ -54,3 +54,17 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+// Add animation when button is clicked
+document.getElementById("addToCart").addEventListener("click", function() {
+// Toggle the 'animated' class on the backpack
+  const backpack = document.getElementById("backpackIcon");
+  backpack.classList.toggle("animated");
+  
+  // Return to the normal size after a time delay
+  setTimeout(function() {
+    backpack.classList.toggle("return-to-normal");
+  }, 500);
+
+});
+
