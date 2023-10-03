3452c8be2c3ddeabe420213eb64753b1af6dcaa3
Amethyst Gilborn <jujaritchie@gmail.com>
2023-10-03 11:50:23 -0600

    Backpack animation try 2

--- src/js/product.js
+++ src/js/product.js
@@ -3,3 +3,16 @@ import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
 productDetails(productId, ".product-detail");
+
+// // Add animation when button is clicked
+// document.getElementById("addToCart").addEventListener("click", function() {
+//     // Toggle the 'animated' class on the backpack
+//       const backpack = document.getElementById("backpackIcon");
+//       backpack.classList.toggle("animated");
+      
+//       // Return to the normal size after a time delay
+//       setTimeout(function() {
+//         backpack.classList.toggle("return-to-normal");
+//       }, 500);
+    
+//     });
