38ac215de0c410aae25a9189c7a563a0dc8b4e00
maddiesinger <hor19013@byui.edu>
2023-10-26 10:16:52 -0600

    fix site

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -19,8 +19,8 @@
     <ul class="product-list">
         {#each filterTents(products) as product}
             <li class="product-card">
-                <a href="product_pages/index.html?product={product.Id}">
-                    <img src={product.Images.PrimarySmall} alt="Image of {product.Name}" />
+                <a href="/product_pages/index.html?product={product.Id}">
+                    <img src={product.Images.PrimaryLarge} alt="Image of {product.Name}" />
                     <h3 class="card__brand">{product.Brand.Name}</h3>
                     <h2 class="card__name">{product.NameWithoutBrand}</h2>
                     <p class="product-card__price">${product.SuggestedRetailPrice}</p>
