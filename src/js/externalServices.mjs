01dbe1df18f7e7125905ae3aac3d5d4f937a3102
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-11-03 16:38:29 -0600

    recommended products and fix breadcrumb

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -13,7 +13,18 @@ export async function getProductsByCategory(category) {
   const data = await convertToJson(response);
   return data.Result;
 }
-
+export async function findRandomProduct() {
+    const categories = ["tents", "sleeping-bags", "hammocks", "backpacks"];
+    const randomCategoryIndex = Math.floor(Math.random() * categories.length);
+    const category = categories[randomCategoryIndex];
+    const productData = await getProductsByCategory(category)    
+    const productIds = productData.map(product => product.Id);
+    const randomIndex = Math.floor(Math.random() * productIds.length);
+    const randomProductId = productIds[randomIndex];
+    const randomProduct = await findProductById(randomProductId)
+    return randomProduct;
+  
+}
 export async function findProductById(id) {
   const response = await fetch(baseURL + `product/${id}`);
   const product = await convertToJson(response);
